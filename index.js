f3878f1966d5b8c67d7c4c39985c46f36713e9c7
Pratham Barua Nabo <nabobarua2@gmail.com>
2024-04-06 20:46:53 +0600

    room Collection implemented

--- index.js
+++ index.js
@@ -45,6 +45,13 @@ async function run() {
       res.send(result)
     })  
 
+    //adding rooms into the database
+    app.post('/rooms',async(req,res)=>{
+      const roomData = req.body;
+      const result = await roomsCollection.insertOne(roomData)
+      res.send(result)
+    })
+
 
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 })
