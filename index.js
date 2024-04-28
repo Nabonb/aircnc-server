68e4391db1bea3d0fabf6d8caee0fe7fd1a04cbc
Pratham Barua Nabo <nabobarua2@gmail.com>
2024-04-28 16:43:52 +0600

    token setup done

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const morgan = require('morgan')
+const jwt = require('jsonwebtoken')
 require('dotenv').config()
 const port = process.env.PORT || 5000
 
@@ -32,6 +33,14 @@ async function run() {
     const roomsCollection = client.db('aircncDb').collection('rooms')
     const bookingsCollection = client.db('aircncDb').collection('bookings')
 
+    //create jwt token
+    app.post('/jwt',(req,res)=>{
+      const email = req.body
+      console.log(email)
+      const token = jwt.sign(email,process.env.ACCESS_TOKEN_SECRET,{expiresIn:"1h"})
+      console.log(token)
+      res.send({token})
+    })
 
     //save user email and role into the mongodb userCollection
     app.put('/users/:email',async(req,res)=>{
